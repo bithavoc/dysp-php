f36e796e6e8e446c842f9515135dc66435a9b9de
thepumpkin1979 <thepumpkin1979@e2ca2434-f039-0410-acca-cbe847b0addc>
2007-09-13 02:01:10 +0000

    dybo Connections and Transactions Added

--- src/main.php
+++ src/main.php
@@ -8,12 +8,14 @@ class Form1 extends dyspForm
 {
 	private $btnSelf;
 	private $btnAuto;
+	private $btnInsert;
 	private $txtName;
 	
 	public function __construct()
 	{
 		parent::__construct("form1",DYSP_KEY);
 	}
+	
 	protected function OnInit()
 	{
 		parent::OnInit();
@@ -32,17 +34,51 @@ class Form1 extends dyspForm
 		$this->Add($this->btnAuto);
 		$this->btnAuto->SetText("Init");
 		
+		//btnInsert
+		$this->btnInsert = new dyspButton("btnInsert");
+		$this->Add($this->btnInsert);
+		$this->btnInsert->SetText("Insert");
+		$this->btnInsert->SetClick(new dyspEventHandler(&$this,"btninsert_Click"));
+		
 		//txtName
 		$this->txtName = new dyspTextBox("txtName");
 		$this->Add($this->txtName);
-		$this->txtName->SetCssClass("TXT");
-		
-		
+		$this->txtName->SetCssClass("TXT");	
 	}
+	
 	public function btnSelf_Click()
 	{
 		$this->btnSelf->SetText("Cambio!");
 		$this->btnAuto->SetVisible(false);
+		$name = $this->txtName->GetText();
+		$conn = new  dyboPgConnection("host=localhost dbname=db1 user=postgres password=programador");
+$conn->Open();
+$conn->BeginTransaction();
+$cmd = $conn->CreateCommand();
+//$cmd->SetCommandText("Create table table2(id int,parent_id int);");
+//$cmd->SetCommandText("insert into table1(id) values(50);");
+//$cmd->SetCommandText("insert into table2(id,parent_id) values(1,50);");
+$cmd->SetCommandText("insert into names(name) values('$name');");
+
+$cmd->Execute();
+$conn->CommitTransaction();
+$conn->Close();
+	}
+		public function btninsert_Click()
+	{
+		$name = $this->txtName->GetText();
+		$conn = new  dyboPgConnection("host=localhost dbname=db1 user=postgres password=programador");
+		$conn->Open();
+		$conn->BeginTransaction();
+		$cmd = $conn->CreateCommand();
+		//$cmd->SetCommandText("Create table table2(id int,parent_id int);");
+		//$cmd->SetCommandText("insert into table1(id) values(50);");
+		//$cmd->SetCommandText("insert into table2(id,parent_id) values(1,50);");
+		$cmd->SetCommandText("insert into names(name) values('$name');");
+
+		$cmd->Execute();
+		$conn->CommitTransaction();
+		$conn->Close();
 	}
 }
 
@@ -50,8 +86,6 @@ $form1 = new Form1();
 $form1->SetTitle("TForm1");
 $form1->Render();
 
-$conn = new  dyboPgConnection("host=localhost dbname=db1 user=postgres password=programador");
-$conn->Open();
-$conn->Close();
+
 
 ?>
