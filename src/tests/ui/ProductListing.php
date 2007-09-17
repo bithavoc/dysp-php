7709a6c4a6181e46f0ba8471083a2f9526537784
thepumpkin1979 <thepumpkin1979@e2ca2434-f039-0410-acca-cbe847b0addc>
2007-09-17 04:53:49 +0000

    More datagrid goodies like Item,Header styling.

--- src/tests/ui/ProductListing.php
+++ src/tests/ui/ProductListing.php
@@ -36,30 +36,72 @@ function getData()
 class ProductListing extends dyspForm
 {
 	private $list;
+	private $btnSearch;
+	private $txtSearch;
+	
 	public function __construct()
 	{
 		parent::__construct("ProductListing",DYSP_KEY);
-		//$this->EnableCustomRender();
+		$this->EnableCustomRender();
 	}
+	
 	public function OnInit()
 	{
 		parent::OnInit();
+		$this->btnSearch = new dyspButton("btnSearch");
+		$this->btnSearch->SetClick(new dyspEventHandler($this,"btnSearch_Click"));
+		$this->Add($this->btnSearch);
+		$this->btnSearch->SetText("Go!");
+		
+		$this->txtSearch = new dyspTextBox("txtSearch");
+		$this->Add($this->txtSearch);
+		
 		$this->AddHeadItem(new dyspStyleReference("main","ProductsManagement.css"));
 		
 		$this->list = new dyspDataGrid("list");
-		$this->list->SetDataSource(getData());
-		$this->list->AddColumn("id","Id");
-		$this->list->AddColumn("name","Nombre");
 		
-		$newItemStyle = new dyspControlStyle();
-		$newItemStyle->SetCssClass("GridValueBack");
-		$this->list->SetItemStyle($newItemStyle);
+		$this->list->AddColumn("Id","Id");
+		$this->list->AddColumn("Name","Nombre");
+		$priceColumn = $this->list->AddColumn("Price","Precio",null,new dyspControlStyle());
+		$priceColumn->GetItemStyle()->SetTextAlign("right");
+		$this->list->AddColumn("IsDisabled","Desactivado");
 		
-		$newItemStyle = new dyspControlStyle();
-		$newItemStyle->SetCssClass("Grid");
-		$this->list->SetStyle($newItemStyle);
+		$this->list->SetStyle(new dyspControlStyle("Grid"));
+		$this->list->SetHeaderStyle(new dyspControlStyle("GridHeader"));
+		$this->list->SetItemStyle(new dyspControlStyle("GridValueBack"));
 		
 		$this->Add($this->list);
+	}
+	public function OnLoad()
+	{
+		parent::OnLoad();
+		if(!$this->IsPostBack())
+		{
+			$manager = new dyboProductDataManager();
+			
+			$this->list->SetDataSource($manager->GetList());
+		}
+	}
+	function btnSearch_Click()
+	{
+		$manager = new dyboProductDataManager();
+		
+		$this->list->SetDataSource($manager->GetList($this->txtSearch->GetText()));
+	}
+	protected function OnCustomRender()
+	{
+		?>
+		<div style="width=250px">
+		Search: 
+		<? $this->txtSearch->Render(); ?>
+		<? $this->btnSearch->Render(); ?>
+		</div>
+		<div>
+		<? $this->list->Render(); ?>
+		</div>
+		<a href="ProductCreate.php">Create</a>
+		
+		<?
 	}
 }
 $form = new ProductListing();
